cb13778a8b7bc26e27ff418b3185ffc54e1dd3ba
Alex Romero <alexromeroreyes09@gmail.com>
2021-09-15 15:27:01 -0700

    Complete reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
 const reverseString = function(str) {
+  var strArr = str.split('');
+  var reversed = '';
 
+  for (let i = strArr.length - 1; i >= 0; i--) {
+    let currentWord = strArr[i];
+    console.log('Current Letter: ', currentWord);
+    reversed += currentWord;
+  }
+  return reversed;
 };
-
 // Do not edit below this line
 module.exports = reverseString;
